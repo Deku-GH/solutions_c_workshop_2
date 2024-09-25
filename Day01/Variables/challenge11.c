98d86acccf81da31be8fb373deb6059b8e39b325
mouad sertati <mouadzed1@gmail.com>
2024-09-25 17:00:53 +0200

    rename challenge011 to challenge11

---
+++ Day01/Variables/challenge11.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (){
+
+float Surface , longueur , largeur ;
+
+ printf ("ente longueur : \n");
+  scanf("%f",&longueur);
+
+ printf ("ente largeur : \n");
+  scanf("%f",&largeur);
+
+ Surface = longueur * largeur;
+
+ printf("la surface = %f",Surface);
+
+}
