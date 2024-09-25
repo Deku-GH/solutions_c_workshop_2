2ccc97961f3a6d7b9f1091e5461a7703d38f004c
mouad sertati <mouadzed1@gmail.com>
2024-09-25 12:12:43 +0200

    push challennge 6

--- Day01/Variables/challenge06.c
+++ Day01/Variables/challenge06.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <math.h>
 
- int main (){
+int main (){
     float a , b ; 
-      printf("entre de nombre b");
-      scanf("%f ",&a);
+      printf("entre de nombre b :\n" );
+      scanf("%f",&a);
 
-      printf("entre de nombre b");
-      scanf("%f ",&b);
-      printf("a + b = %f",a + b);
-      printf("a - b = %f",a - b);
-      printf("a * b = %f",a * b);
-    printf("a _ b = %f",a/b); 
+      printf("entre de nombre b \n ");
+      scanf("%f",&b);
 
+      printf("a + b = %f \n",a + b);
+      printf("a - b = %f \n",a - b);
+      printf("a * b = %f \n",a * b);
 
+  if (a!=0 && b!= 0 )
+      printf("a / b = %f",a/b); 
 
-
+  else 
+     printf(" la division et impossible ");
+ return 0;
  }
