0accf2b8fff99f32a5c72b568adc09d4bb398e24
mouad sertati <mouadzed1@gmail.com>
2024-09-25 17:32:51 +0200

    push fil challenge12 done

---
+++ Day01/Variables/challenge12.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (){
+ int a , b , c , d ;
+ printf("give a  number : \n");
+ scanf("%d",&a );
+  printf("give a number : \n");
+ scanf("%d",&b);
+   printf("give a number : \n");
+ scanf("%d",&c);
+   printf("give a number : \n");
+ scanf("%d",&d);
+ 
+ 
+
+printf("%d %d %d %d",d , c, b , a);
+
+}
