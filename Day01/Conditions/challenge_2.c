33a92d988e0052d5f10e88d23a2ac95a82c6a32f
mouad sertati <mouadzed1@gmail.com>
2024-09-25 18:29:09 +0200

    push seconde chall in conditons

---
+++ Day01/Conditions/challenge_2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main (){
+
+   char C ;
+   printf("entre un sole caracter : ");
+   scanf("%c",&C);
+    
+    switch (C)
+    {
+        case 'a' :printf("le caratere et voyelle");
+        break;  
+
+        case 'e' :printf("le caratere et voyelle");
+        break;
+
+        case 'u' :printf("le caratere et voyelle");
+        break; 
+
+        case 'i' :printf("le caratere et voyelle");
+        break;
+
+        case 'o' :printf("le caratere et voyelle");
+        break;
+    
+     default: 
+          printf("la caractere et non voyelle ");
+    }
+
+
+
+
+}
