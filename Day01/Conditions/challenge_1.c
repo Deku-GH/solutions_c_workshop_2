5a52092b43d89c76bd1605e32381a42198d13b4b
mouad sertati <mouadzed1@gmail.com>
2024-09-25 18:06:54 +0200

    first challenge in conditions is done

---
+++ Day01/Conditions/challenge_1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (){  
+    int A ;
+    printf("give me number : ");
+    scanf("%d",&A);
+    if (A%2==0)
+        printf("pair");
+    else
+        printf("impair");
+}
+
