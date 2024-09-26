0780e46350c64f98103b0ef70e42a2ace09a6106
mouad sertati <mouadzed1@gmail.com>
2024-09-26 14:32:06 +0200

    add change n chall 5

--- Day01/Conditions/challenge_5.c
+++ Day01/Conditions/challenge_5.c
@@ -2,7 +2,22 @@
 
 
 int main (){
-    int mois , jours , heurs , minutes , seconde      
+    int annee,mois , jours , heurs , minutes , seconde ;
+    printf("entre la nomber de l annee :");
+    scanf("%d",&annee);
+     
+
+     mois = annee * 12 ;
+     jours = annee * 365 ;
+     heurs = annee * 8760 ;
+     minutes = annee * 525600 ;
+     seconde = annee * 31536000 ;
+
+printf ("%d an = %d",annee,mois);
+printf ("%d an = %d",annee,jours);
+printf ("%d an = %d",annee,minutes);
+printf ("%d an = %d",annee,seconde);
+
 
 
 
