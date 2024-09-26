e89a135c323feabcd63c2447e847157956c660b0
mouad sertati <mouadzed1@gmail.com>
2024-09-26 16:32:55 +0200

    tchange in chll 10

--- Day01/Conditions/challenge_10.c
+++ Day01/Conditions/challenge_10.c
@@ -2,7 +2,43 @@
 
 int main (){
      int jj ,mm , nnnn ;
-     printf("");
+     printf("entre la jour :");
+     scanf("%d",&jj);
+
+     printf("entre la jour :");
+     scanf("%d",&mm);   
+
+     printf("entre la jour :");
+     scanf("%d",&nnnn);
+     switch (mm)
+     {
+     case  1 : printf("%d-janvier-%d",jj,nnnn);
+        break;
+     case  2 : printf("%d-février-%d",jj,nnnn);
+        break;
+     case  3 : printf("%d-mars-%d",jj,nnnn);
+        break;
+     case  4 : printf("%d-avril-%d",jj,nnnn);
+        break;
+     case  5 : printf("%d-mai-%d",jj,nnnn);
+        break;
+     case  6 : printf("%d-juin-%d",jj,nnnn);
+        break;
+     case  7 : printf("%d-juillet-%d",jj,nnnn);
+        break;
+     case  8 : printf("%d-aout-%d",jj,nnnn);
+        break;
+     case  9 : printf("%d-septembre-%d",jj,nnnn);
+        break;
+     case  10 : printf("%d-octobre-%d",jj,nnnn);
+        break;
+     case  11 : printf("%d-novembre-%d",jj,nnnn);
+        break;
+     case  12 : printf("%d-décembre-%d",jj,nnnn);
+        break;
+     default:printf("you have errore");
+        break;
+     }
 
 
 }
