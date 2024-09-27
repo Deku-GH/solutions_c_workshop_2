ea5406a5d46e226ca3d950921933241a519ae928
mouad sertati <mouadzed1@gmail.com>
2024-09-27 11:25:56 +0200

    corect chall 5

--- Day01/Conditions/challenge_5.c
+++ Day01/Conditions/challenge_5.c
@@ -2,10 +2,13 @@
 
 
 int main (){
-    int annee,mois , jours , heurs , minutes , seconde ;
+    int annee,mois , jours , heurs , minutes , seconde ,name ;
+    
     printf("entre la nomber de l annee :");
-    scanf("%d",&annee);
-     
+    scanf("%d",&annee );
+
+    printf("%d annee = mois = 1 jours = 2 heurs = 3 seconde = 4 ",annee);
+    scanf("%d",&name);
 
      mois = annee * 12 ;
      jours = annee * 365 ;
@@ -13,15 +16,20 @@ int main (){
      minutes = annee * 525600 ;
      seconde = annee * 31536000 ;
 
-printf ("%d an = %d",annee,mois);
-printf ("%d an = %d",annee,jours);
-printf ("%d an = %d",annee,minutes);
-printf ("%d an = %d",annee,seconde);
 
-
-
-
-
+switch (name)
+{
+ case   1 : printf ("%d an = %d",annee,mois);
+     break;
+ case 2 :printf ("%d an = %d",annee,jours);
+     break;
+  case 3: printf ("%d an = %d",annee,minutes);
+     break;
+  case 4 :printf ("%d an = %d",annee,seconde);
+     break;
+  default: 
+      break;
+}
 
 
 }
