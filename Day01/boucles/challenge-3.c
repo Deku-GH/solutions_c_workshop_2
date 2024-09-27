03fa92ea1172b8feb8602b3aa83f7be78ba80e86
mouad sertati <mouadzed1@gmail.com>
2024-09-27 18:04:18 +0200

    add chall 4\5

---
+++ Day01/boucles/challenge-3.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+int main  (){
+int i ,n;
+printf("entre a nombre :");
+scanf("%d",&n);
+for(i=n-1;i >= 1;i--){
+    n = n + i ;
+
+}
+printf (" la somme des premiers nombres naturels  = %d",n);
+    return 0;
+}
