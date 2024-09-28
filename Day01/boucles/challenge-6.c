d71f00f5faf6059abafe84fe47597920600aae65
mouad sertati <mouadzed1@gmail.com>
2024-09-28 11:05:14 +0200

    add chall 5\6

--- Day01/boucles/challenge-6.c
+++ Day01/boucles/challenge-6.c
@@ -5,7 +5,7 @@ int n  ;
  printf("entre a nombre :");
 scanf("%d",&n);
 for ( int i= 0 ; i <=9;i++)
-   if (n%2 !=0) {
-   if (i % 2 != 0)
+   if (n%2 ==0) {
+   if (i % 2 == 0)
     printf("%d",i); }
 }
