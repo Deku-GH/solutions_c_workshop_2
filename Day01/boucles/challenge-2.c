3d3c9a656f82620d36a24e3555dd3155fcb915e5
mouad sertati <mouadzed1@gmail.com>
2024-09-28 12:46:38 +0200

    corecte

--- Day01/boucles/challenge-2.c
+++ Day01/boucles/challenge-2.c
@@ -6,10 +6,11 @@ printf("entre a nombre :");
 scanf("%d",&s);
 for(i=s-1;i >= 1;i--){
     s = s * i ;
-    printf("%d * %d= %d \n",s,i,s * i);
+    // printf("%d * %d= %d \n",s,i,s * i);
      
     
 }
 printf (" calcule la factorielle d'un nombre entier positif = %d",s);
     return 0;
+
 }
